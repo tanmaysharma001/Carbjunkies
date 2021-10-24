3ccc7ea6e11c578b07c35a5fcd62011626604dfa
Tanmay Sharma <tanmaysharma028@gmail.com>
2021-10-24 18:44:34 +0530

    make it better

--- src/main/java/com/tanmay/carbjunkies/Controller/RecipeController.java
+++ src/main/java/com/tanmay/carbjunkies/Controller/RecipeController.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import sun.lwawt.macosx.CSystemTray;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Controller
